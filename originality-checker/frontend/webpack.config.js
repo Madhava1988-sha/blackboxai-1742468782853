72ab4b9c63df9f14394cbcd6d61a8dcd4513510f
Madhava1988-sha <madhavashankar.r1988@gmail.com>
2025-03-20 16:37:26 +0530

    Add originality-checker/frontend/webpack.config.js 1742468845482

---
+++ originality-checker/frontend/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public/dist'),
+    filename: 'bundle.js',
+    publicPath: '/dist/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react']
+          }
+        }
+      }
+    ]
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    port: 8000,
+    hot: true,
+    open: true
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+};
